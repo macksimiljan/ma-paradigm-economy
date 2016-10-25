3271854e821923f1c6c6fc4c42a2d443fc967a38
macksimiljan <macksimiljan@users.noreply.github.com>
2016-10-25 12:49:33 +0200

    [refactor] changed generator test to Nuer

--- src/test/java/simulation/GeneratorTest.java
+++ src/test/java/simulation/GeneratorTest.java
@@ -1,7 +1,11 @@
 package simulation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,24 +27,32 @@ public class GeneratorTest {
 	@Test
 	public void test_calcICSs() throws IllegalAccessException {
 		AllomorphDistribution alloDistr = new AllomorphDistribution();
-		String[] exponents1 = {"a1", "a2", "a3"};
-		String[] exponents2 = {"b1", "b2", "b3", "b4"};
-		String[] exponents3 = {"c1", "c2"};
-		alloDistr.addExponents("101", new HashSet<String>(Arrays.asList(exponents1)));
-		alloDistr.addExponents("23", new HashSet<String>(Arrays.asList(exponents2)));
-		alloDistr.addExponents("30", new HashSet<String>(Arrays.asList(exponents3)));
+		String[] exponents1 = {"{ }"};
+		String[] exponents2 = {"{ }", "kä", "ä"};
+		String[] exponents3 = {"{ }", "ni"};
+		alloDistr.addExponents("nom.sg", new HashSet<String>(Arrays.asList(exponents1)));
+		alloDistr.addExponents("gen.sg", new HashSet<String>(Arrays.asList(exponents2)));
+		alloDistr.addExponents("loc.sg", new HashSet<String>(Arrays.asList(exponents2)));
+		alloDistr.addExponents("nom.pl", new HashSet<String>(Arrays.asList(exponents3)));
+		alloDistr.addExponents("gen.pl", new HashSet<String>(Arrays.asList(exponents3)));
+		alloDistr.addExponents("loc.pl", new HashSet<String>(Arrays.asList(exponents3)));
 		
 		Set<InflectionClass> classes = Generator.getAllInflectionClasses(alloDistr);
 		System.out.println("|classes|: "+classes.size());
 		
-//		Set<InflectionClassSystem> systems = Generator.getInflectionClassSystems(classes, alloDistr, 4);
+		Set<InflectionClassSystem> systems = Generator.getInflectionClassSystems(classes, alloDistr, 3);
 		
-		Set<InflectionClassSystem> systems = Generator.getInflectionClassSystems(classes, alloDistr);
+//		Set<InflectionClassSystem> systems = Generator.getInflectionClassSystems(classes, alloDistr);
 
-		System.out.println("|systems|: "+systems.size());
-//		for (InflectionClassSystem ics : systems) {
-//			System.out.println(ics);
-//		}
+//		System.out.println("|systems|: "+systems.size());
+		
+		try (PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter("./nuer3.txt")))){
+			for (InflectionClass c : classes)
+				w.println(c);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 
